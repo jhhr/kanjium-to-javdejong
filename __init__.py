742b2d7ac5bb5a6a51a2752d092716f2ce938207
jhhr <jormki@gmail.com>
2023-12-29 20:31:58 +0200

    Add bulk convert browser menu action

--- __init__.py
+++ __init__.py
@@ -1,5 +1,14 @@
 from anki import hooks
 from aqt import mw
+from aqt.browser import Browser
+from anki.notes import Note, NoteId
+from aqt import gui_hooks
+from aqt.qt import QAction, qconnect
+from aqt.operations import CollectionOp
+from aqt.utils import showInfo
+
+
+from collections.abc import Sequence
 
 import re
 
@@ -93,7 +102,7 @@ def convert_kanjium_to_javdejong(kanjium_string):
 
 
 # Function to be executed when the add_cards_did_add_note hook is triggered
-def on_add_card_convert_pitch_accent_notation(_col, note, _deck_id):
+def convert_pitch_accent_notation_in_note(note: Note):
     # Check if the note has the 'vocab-pitch-pattern' field
     if 'vocab-pitch-pattern' in note:
         if debug:
@@ -111,5 +120,39 @@ def on_add_card_convert_pitch_accent_notation(_col, note, _deck_id):
             # Update the note with the modified 'vocab-pitch-pattern' value
             note['vocab-pitch-pattern'] = modified_pitch_pattern
 
-# Register to hook
-hooks.note_will_be_added.append(on_add_card_convert_pitch_accent_notation)
+def bulk_convert_notes_op(col, notes: Sequence[Note]):
+    pos = col.add_custom_undo_entry(f"Convert pitch accent notation for {len(notes)} notes.")
+    for note in notes:
+        convert_pitch_accent_notation_in_note(note)
+    col.update_notes(notes)
+    return col.merge_undo_entries(pos)
+
+
+# Function to be executed when the "Convert Pitch Accent Notation" menu action is triggered
+def convert_selected_notes(nids: Sequence[NoteId], parent: Browser):
+       CollectionOp(
+        parent=parent, op=lambda col: bulk_convert_notes_op(col, notes=[mw.col.get_note(nid) for nid in nids])
+    ).success(
+        lambda out: showInfo(
+            parent=parent,
+            title="Conversion done",
+            textFormat="rich",
+            text=f"Converted pitch accent notation in {len(nids)} selected notes."
+        )
+    ).run_in_background()
+
+
+# Function to be executed when the browser menus are initialized
+def on_browser_menus_did_init(browser: Browser):
+    # Create a new action for the browser menu
+    action = QAction("Convert Pitch Accent Notation", mw)
+    # Connect the action to the convert_selected_notes function
+    qconnect(action.triggered, lambda: convert_selected_notes(browser.selectedNotes(), parent=browser))
+    # Add the action to the browser's card context menu
+    browser.form.menuEdit.addAction(action)
+
+# Register to card adding hook
+hooks.note_will_be_added.append(lambda _col, note, _deck_id: convert_pitch_accent_notation_in_note(note))
+
+# Register to browser menu initialization hook
+gui_hooks.browser_menus_did_init.append(on_browser_menus_did_init)
